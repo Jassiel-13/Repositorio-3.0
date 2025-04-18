bce0ea789fc82d32c6c2c2c00cc647f4dec7ab39
Jassiel-13 <l21210360@tectijuana.edu.mx>
2025-04-17 22:28:06 -0700

    Primer commit

---
+++ Proyecto Hilos U3/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Proyecto_Hilos_U3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.btn_Iniciar = new System.Windows.Forms.Button();
+            this.btn_Pausar = new System.Windows.Forms.Button();
+            this.btn_Reanudar = new System.Windows.Forms.Button();
+            this.btn_Reiniciar = new System.Windows.Forms.Button();
+            this.lblganador = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Iniciar
+            // 
+            this.btn_Iniciar.Location = new System.Drawing.Point(12, 12);
+            this.btn_Iniciar.Name = "btn_Iniciar";
+            this.btn_Iniciar.Size = new System.Drawing.Size(74, 23);
+            this.btn_Iniciar.TabIndex = 2;
+            this.btn_Iniciar.Text = "INICIAR";
+            this.btn_Iniciar.UseVisualStyleBackColor = true;
+            this.btn_Iniciar.Click += new System.EventHandler(this.btn_Iniciar_Click);
+            // 
+            // btn_Pausar
+            // 
+            this.btn_Pausar.Location = new System.Drawing.Point(93, 12);
+            this.btn_Pausar.Name = "btn_Pausar";
+            this.btn_Pausar.Size = new System.Drawing.Size(74, 23);
+            this.btn_Pausar.TabIndex = 2;
+            this.btn_Pausar.Text = "PAUSAR";
+            this.btn_Pausar.UseVisualStyleBackColor = true;
+            this.btn_Pausar.Click += new System.EventHandler(this.btn_Pausar_Click);
+            // 
+            // btn_Reanudar
+            // 
+            this.btn_Reanudar.Location = new System.Drawing.Point(174, 12);
+            this.btn_Reanudar.Name = "btn_Reanudar";
+            this.btn_Reanudar.Size = new System.Drawing.Size(83, 23);
+            this.btn_Reanudar.TabIndex = 2;
+            this.btn_Reanudar.Text = "REANUDAR";
+            this.btn_Reanudar.UseVisualStyleBackColor = true;
+            this.btn_Reanudar.Click += new System.EventHandler(this.btn_Reanudar_Click);
+            // 
+            // btn_Reiniciar
+            // 
+            this.btn_Reiniciar.Location = new System.Drawing.Point(264, 12);
+            this.btn_Reiniciar.Name = "btn_Reiniciar";
+            this.btn_Reiniciar.Size = new System.Drawing.Size(74, 23);
+            this.btn_Reiniciar.TabIndex = 2;
+            this.btn_Reiniciar.Text = "REINICIAR";
+            this.btn_Reiniciar.UseVisualStyleBackColor = true;
+            this.btn_Reiniciar.Click += new System.EventHandler(this.btn_Reiniciar_Click);
+            // 
+            // lblganador
+            // 
+            this.lblganador.AutoSize = true;
+            this.lblganador.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblganador.Location = new System.Drawing.Point(626, 12);
+            this.lblganador.Name = "lblganador";
+            this.lblganador.Size = new System.Drawing.Size(0, 25);
+            this.lblganador.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 453);
+            this.Controls.Add(this.lblganador);
+            this.Controls.Add(this.btn_Reiniciar);
+            this.Controls.Add(this.btn_Reanudar);
+            this.Controls.Add(this.btn_Pausar);
+            this.Controls.Add(this.btn_Iniciar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Proyecto Hilos - U3 Castro Hernandez Rolando Jassiel -Fansisco Alonso Robles Barr" +
+    "era";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button btn_Iniciar;
+        private System.Windows.Forms.Button btn_Pausar;
+        private System.Windows.Forms.Button btn_Reanudar;
+        private System.Windows.Forms.Button btn_Reiniciar;
+        private System.Windows.Forms.Label lblganador;
+    }
+}
+
---
+++ Proyecto Hilos U3/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Hilos_U3
+{
+    public partial class Form1 : Form
+    {
+        private int[] posicionesX = { 2, 2, 2, 2, 2 };
+        private int[] posicionesY = { 50, 130, 210, 290, 370 };
+        private PictureBox[] aviones = new PictureBox[5];
+        private Button[] botones = new Button[5];
+        private List<Thread> hilos = new List<Thread>();
+        private Random random = new Random();
+        private ManualResetEvent pausa = new ManualResetEvent(true);
+        private bool carreraTerminada = false;
+        private Label lblGanador;
+        private PictureBox fondo;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ConfigurarDobleBuffer();
+            CargarPista();
+            CargarAviones();
+            CargarBotones();
+            CargarLabelGanador();
+            ReproducirMusica("C:\\Users\\Jassiel\\source\\repos\\Proyecto Hilos U3\\Sonidos\\Screen.wav");
+        }
+
+        private void ConfigurarDobleBuffer()
+        {
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+        }
+
+        private void CargarPista()
+        {
+            string rutaPista = Path.Combine(Application.StartupPath, "C:\\Users\\Jassiel\\source\\repos\\Proyecto Hilos U3\\Imagenes", "pista2.png");
+            if (File.Exists(rutaPista))
+            {
+                fondo = new PictureBox()
+                {
+                    Image = Image.FromFile(rutaPista),
+                    Dock = DockStyle.Fill,
+                    SizeMode = PictureBoxSizeMode.StretchImage
+                };
+                this.Controls.Add(fondo);
+                fondo.SendToBack(); // Asegura que la pista no tape otros elementos
+            }
+        }
+
+        private void CargarAviones()
+        {
+            string rutaAvion = Path.Combine(Application.StartupPath, "C:\\Users\\Jassiel\\source\\repos\\Proyecto Hilos U3\\Imagenes", "avion.png");
+            this.SuspendLayout();
+            for (int i = 0; i < 5; i++)
+            {
+                if (File.Exists(rutaAvion))
+                {
+                    aviones[i] = new PictureBox()
+                    {
+                        Image = Image.FromFile(rutaAvion),
+                        SizeMode = PictureBoxSizeMode.Zoom,
+                        Size = new Size(50, 50),
+                        Location = new Point(posicionesX[i], posicionesY[i]),
+                        BackColor = Color.Transparent
+                    };
+                    this.Controls.Add(aviones[i]);
+                    aviones[i].BringToFront();
+                }
+                else
+                {
+                    MessageBox.Show("No se encontró la imagen del avión: " + rutaAvion);
+                }
+            }
+            this.ResumeLayout();
+        }
+
+        private void CargarBotones()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                botones[i] = new Button()
+                {
+                    Text = $"Avión {i + 1}",
+                    Location = new Point(20, posicionesY[i]),
+                    Size = new Size(80, 30),
+                    BackColor = Color.LightBlue
+                };
+                botones[i].Click += (sender, e) => MessageBox.Show($"Avión {i + 1} listo!");
+                this.Controls.Add(botones[i]);
+                botones[i].BringToFront();
+            }
+        }
+
+        private void CargarLabelGanador()
+        {
+            lblGanador = new Label()
+            {
+                Text = "",
+                Location = new Point(390, 10),
+                Font = new Font("Arial", 14, FontStyle.Bold),
+                AutoSize = true
+            };
+            this.Controls.Add(lblGanador);
+        }
+
+        private void btn_Iniciar_Click(object sender, EventArgs e)
+        {
+            carreraTerminada = false;
+            lblGanador.Text = "";
+            hilos.Clear();
+
+            for (int i = 0; i < 5; i++)
+            {
+                posicionesX[i] = 2;
+                aviones[i].Location = new Point(posicionesX[i], posicionesY[i]);
+                int index = i;
+                Thread hilo = new Thread(() => MoverAvion(index));
+                hilos.Add(hilo);
+                hilo.Start();
+            }
+            ReproducirMusica("C:\\Users\\Jassiel\\source\\repos\\Proyecto Hilos U3\\Sonidos\\Despegue.wav");
+        }
+
+        private void MoverAvion(int index)
+        {
+            int velocidad = random.Next(3, 30);
+            while (posicionesX[index] < 800 && !carreraTerminada)
+            {
+                pausa.WaitOne();
+                Thread.Sleep(velocidad);
+                posicionesX[index] += 6;
+                this.Invoke((MethodInvoker)delegate
+                {
+                    aviones[index].Location = new Point(posicionesX[index], posicionesY[index]);
+                    VerificarGanador(index);
+                    aviones[index].Invalidate();
+                });
+            }
+        }
+
+        private void VerificarGanador(int index)
+        {
+            if (!carreraTerminada && posicionesX[index] >= 800)
+            {
+                carreraTerminada = true;
+                lblGanador.Text = $"🏆 ¡GANADOR: Avión {index + 1}! 🏆";
+                lblGanador.Visible = true;
+                lblGanador.BringToFront();
+                ReproducirMusica("C:\\Users\\Jassiel\\source\\repos\\Proyecto Hilos U3\\Sonidos\\Score.wav");
+            }
+        }
+
+        private void btn_Pausar_Click(object sender, EventArgs e)
+        {
+            pausa.Reset();
+        }
+
+        private void btn_Reanudar_Click(object sender, EventArgs e)
+        {
+            pausa.Set();
+        }
+
+        private void btn_Reiniciar_Click(object sender, EventArgs e)
+        {
+            carreraTerminada = true;
+            pausa.Reset();
+            foreach (Thread hilo in hilos)
+            {
+                if (hilo.IsAlive)
+                {
+                    hilo.Abort();
+                }
+            }
+            btn_Iniciar_Click(null, null);
+        }
+
+        private void ReproducirMusica(string archivo)
+        {
+            try
+            {
+                string rutaSonido = Path.Combine(Application.StartupPath, archivo);
+                if (File.Exists(rutaSonido))
+                {
+                    using (SoundPlayer sp = new SoundPlayer(rutaSonido))
+                    {
+                        sp.Play();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error: No se encontró el archivo de sonido " + archivo);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar el sonido: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
